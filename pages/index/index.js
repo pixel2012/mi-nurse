5cf198ea2e14cb6fdb2c44acf63dab9e94882131
李虎 <lihu20120810@163.com>
2018-05-18 23:11:59 +0800

    完善切图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,16 @@
 var app = getApp();
+var wxCharts = require('../../common/js/wxcharts.js');
+var lineChart = null;
 Page({
   data: {},
   onLoad() {
+
+    this.lineInit();
+  },
+  onShow() {
+
+  },
+  bluetoothInit: function () {
     //检测蓝牙是否打开
     wx.openBluetoothAdapter({
       success: function (res) {
@@ -66,14 +75,76 @@ Page({
       fail: function (res) {
         console.log('蓝牙未打开');
       }
-    })
-
-
-
-
-
+    });
   },
-  onShow() {
+  lineInit: function () {
+    var windowWidth = 320;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth - 20;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
 
-  }
+    var simulationData = this.createSimulationData();
+    lineChart = new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: simulationData.categories,
+      animation: true,
+      // background: '#f5f5f5',
+      series: [{
+        name: '成交量1',
+        data: simulationData.data,
+        format: function (val, name) {
+          return val.toFixed(2) + '万';
+        }
+      }, {
+        name: '成交量2',
+        data: [2, 0, 0, 3, null, 4, 0, 0, 2, 0],
+        format: function (val, name) {
+          return val.toFixed(2) + '万';
+        }
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: '成交金额 (万元)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 0
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
+  },
+  touchHandler: function (e) {
+    console.log(lineChart.getCurrentDataIndex(e));
+    lineChart.showToolTip(e, {
+      // background: '#7cb5ec',
+      format: function (item, category) {
+        return category + ' ' + item.name + ':' + item.data
+      }
+    });
+  },
+  createSimulationData: function () {
+    var categories = [];
+    var data = [];
+    for (var i = 0; i < 10; i++) {
+      categories.push('2016-' + (i + 1));
+      data.push(Math.random() * (20 - 10) + 10);
+    }
+    // data[4] = null;
+    return {
+      categories: categories,
+      data: data
+    }
+  },
 });
