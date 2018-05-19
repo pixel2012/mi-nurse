a95240450749592e33d34394ba836e7440d659dd
李虎 <lihu20120810@163.com>
2018-05-19 22:44:42 +0800

    智能美胸

--- pages/breast/index.js
+++ pages/breast/index.js
@@ -1,6 +1,20 @@
 var app = getApp();
 Page({
-  data: {},
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    current:0,
+    indicatorDots: true,
+    indicatorActiveColor:'#12C8C8',
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    circular:true,
+    displayMultipleItems:2
+  },
   onLoad(){
 
   },
