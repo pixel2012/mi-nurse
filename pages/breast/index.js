89131cb07800720f9a69f03f310ed3b861d5caef
李虎 <lihu20120810@163.com>
2018-05-22 23:40:02 +0800

    智能美胸绑定事件

--- pages/breast/index.js
+++ pages/breast/index.js
@@ -13,12 +13,78 @@ Page({
     interval: 5000,
     duration: 1000,
     circular:true,
-    displayMultipleItems:2
+    displayMultipleItems:2,
+    tabIndex:0,//当前高亮的tab标签栏
+    isMenu:false,//是否显示菜单
+    menuIndex:0,//当前高亮的菜单项
+    menuList:[
+      {
+        id:0,
+        title:'盛夏\n如花'
+      },
+      {
+        id: 2,
+        title: '高山\n流水'
+      },
+      {
+        id: 3,
+        title: '阵阵\n酥麻'
+      },
+      {
+        id: 4,
+        title: '小鹿\n乱撞'
+      },
+      {
+        id: 5,
+        title: '浪花\n迭起'
+      },
+      {
+        id: 6,
+        title: '琴瑟\n长鸣'
+      },
+      {
+        id: 7,
+        title: '随机'
+      }
+    ],
+    play:false,//震动状态
+    playTitle:'',
+    strength:1,//震动强度
   },
   onLoad(){
 
   },
   onShow(){
     
+  },
+  bindTab(e){
+    this.setData({
+      tabIndex: e.currentTarget.dataset.index
+    });
+  },
+  showMenu(){
+    this.setData({
+      isMenu:true
+    });
+  },
+  hideMenu(){
+    this.setData({
+      isMenu: false
+    });
+  },
+  showMenuItem(e){
+    this.setData({
+      menuIndex: e.currentTarget.dataset.index
+    });
+  },
+  bindPlay(){
+    this.setData({
+      play:!this.data.play
+    });
+  },
+  bindStrength(e){
+    this.setData({
+      strength: e.currentTarget.dataset.index
+    });
   }
 });
