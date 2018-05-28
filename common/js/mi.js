d335d4a41a541e45cf504e14d8b8206450ca0a6a
李虎 <lihu20120810@163.com>
2018-05-28 23:21:59 +0800

    丰富完善ajax和store

--- common/js/mi.js
+++ common/js/mi.js
@@ -1,71 +1,173 @@
 let crypto = require('./crypto-js.min');
-const secretKey = 'abcdefgabcdefg12';
+const aesKey = 'u.!4pb.pLzh^sN)u';
 let mi = {
-    version: '0.0.1',
-    ip: 'http://api.51mito.com/api/',
-    isAuth(){
+  version: '0.0.1',
+  ip: 'http://api.51mito.com/api/',
+  ajax: function (param) {
+    var that = this;
+    //默认请求方式为get
+    if (!params.hasOwnProperty('method') || !params.method) {
+      params.method = 'GET';
+    }
+    let myToken = store.get('myToken');
+    let myRefreshToken = store.get('myRefreshToken');
+    let userinfo = store.get('userinfo');
 
-    },
-    ajax: function (param) {
-        wx.request({
-            url: param.url,
-            header: {
-                uid: '',
-                token: ''
-            },
-            data: param.data,
-            success: function () {
-            },
-            fail: function () {
-            },
-            complete: function () {
-            }
+    if (!access_token && params.login) {
+      request({
+        url: this.ip + '/user/refreshToken',
+        method: 'post',
+        data: {
+          myId: "18",
+          myRefreshToken: "b4db6ce6-d6ae-1394-9139-73403063f7a4",
+          pushChannelId: "42739732949249670",
+          os: "android/ios"
+        },
+        success: function (data) {
+          if (data) {
+            //1，存储到本地
+            data.forEach(v => {
+              that.store.set(v, data[v]);
+            });
+            //2，请求接口
+            request(params, data.myToken);
+          }
+        },
+        fail: function () {
+          wx.showModal({
+            title: '提示',
+            content: "刷新myRefreshToken失败！"
+          });
+        }
+      });
+    } else {
+      request(params, data.myToken || '');
+    }
+
+    function request(params, token) {
+      if (!params.hasOwnProperty('loading') || params.loading) {
+        wx.showLoading({
+          title: '努力加载中...',
         });
-    },
-    store: {
-        set: function (key, data) {
-            try {
-                wx.setStorageSync(key, data);
-            } catch (e) {
-                console.error(JSON.stringify(e));
-            }
-        },
-        get: function (key) {
-            try {
-                return wx.getStorageSync(key);
-            } catch (e) {
-                console.error(JSON.stringify(e));
-            }
-        },
-        remove: function (key) {
-            try {
-                wx.removeStorageSync(key);
-            } catch (e) {
-                console.error(JSON.stringify(e));
-            }
-        },
-        clear: function () {
-            try {
-                wx.clearStorageSync();
-            } catch (e) {
-                console.error(JSON.stringify(e));
-            }
+      }
+      //判断参数是写到url里面还是body里面
+      var Pos = 0; //0是不写任何参数，1是写到url里，2是写道body里
+      if (params.hasOwnProperty('data')) {
+        if (params.method == 'GET' || !params.hasOwnProperty('dataPos') || (params.hasOwnProperty('dataPos') && params.dataPos)) {
+          Pos = 1; //params中方法是get的或者没有dataPos或者dataPos为true时，参数写道url里面
+        } else {
+          Pos = 2; //参数写道body里面
         }
-    },
-    crypto: {
-        encode: function (word) {
-            let key = crypto.enc.Utf8.parse(secretKey);
-            let result = crypto.enc.Utf8.parse(word);
-            let encrypted = crypto.AES.encrypt(result, key, {mode: crypto.mode.ECB, padding: crypto.pad.Pkcs7});
-            // var encryptedStr = encrypted.ciphertext.toString();
-            return encrypted.toString();
-        },
-        decode: function (word) {
-            let key = crypto.enc.Utf8.parse(secretKey);
-            let result = crypto.AES.decrypt(word, key, {mode: crypto.mode.ECB, padding: crypto.pad.Pkcs7});
-            return crypto.enc.Utf8.stringify(result).toString();
+      }
+      var urlData = params.url + '?'; //url地址
+      if (Pos == 1) {
+        for (var item in params.data) {
+          urlData += item + '=' + params.data[item] + '&';
         }
+      }
+      var sendData = ''; //定义将要上传服务器的数据
+      /*当含有加密参数开启*/
+      if (params.hasOwnProperty('encrypt') && params.encrypt) {
+        sendData = that.crypto.encode(JSON.stringify(params.data));
+      } else {
+        sendData = params.data;
+      }
+      wx.request({
+        url: urlData.slice(0, urlData.length - 1),
+        method: params.method,
+        header: {
+          uid: store.get('uid'),
+          token: token
+        },
+        data: sendData,
+        success: function (res) {
+          if (res.status) {
+            //成功回调
+            if (params.callback) {
+              params.callback(res.data.data);
+            }
+          } else {
+            if (!params.hasOwnProperty('failTip') || params.failTip) {
+              wx.showModal({
+                title: '提示',
+                content: res.data.message || "未知错误"
+              });
+            }
+            //报错回调
+            if (params.failback) {
+              params.failback(res.data.data);
+            }
+          }
+        },
+        fail: function (res) {
+          if (!params.hasOwnProperty('errorTip') || params.errorTip) {
+            wx.showModal({
+              title: '提示',
+              content: "url:" + params.url.slice(0, params.url.length) + "\n data:" + JSON.stringify(res)
+            });
+          }
+
+        },
+        complete: function (res) {
+          if (!params.hasOwnProperty('loading') || params.loading) {
+            wx.hideLoading()
+          }
+          if (params.finalback) {
+            params.finalback(res);
+          }
+        }
+      });
+    }
+  },
+  store: {
+    set: function (key, data, expire) {
+      wx.setStorageSync(key, data);
+      //如果传入了过期时间，则追加一个参数存储过期时间
+      if (expires) {
+        setTimeout(function () {
+          wx.setStorageSync(key + '_expires', new Date().getTime() + expires);//单位毫秒
+        }, 0);
+      }
     },
+    get: function (key) {
+      var value = '';//获取的值
+      var content = wx.getStorageSync(key);
+      var expires = wx.getStorageSync(key + '_expires');
+      if (expires) {
+        //如果有过期时间
+        if (expires - new Date().getTime() > 0) {
+          return content
+        } else {
+          wx.removeStorageSync(key);
+          wx.removeStorageSync(key + '_expires');
+          return '';
+        }
+      } else {
+        //没有过期时间
+        return content
+      }
+    },
+    remove: function (key) {
+      wx.removeStorageSync(key);
+    },
+    clear: function () {
+      wx.clearStorageSync();
+    }
+  },
+  crypto: {
+    encode: function (word) {
+      let key = crypto.enc.Utf8.parse(aesKey);
+      let result = crypto.enc.Utf8.parse(word);
+      let encrypted = crypto.AES.encrypt(result, key, { mode: crypto.mode.ECB, padding: crypto.pad.Pkcs7 });
+      // var encryptedStr = encrypted.ciphertext.toString();
+      return encrypted.toString();
+    },
+    decode: function (word) {
+      let key = crypto.enc.Utf8.parse(aesKey);
+      let result = crypto.AES.decrypt(word, key, { mode: crypto.mode.ECB, padding: crypto.pad.Pkcs7 });
+      return crypto.enc.Utf8.stringify(result).toString();
+    }
+  },
 
 };
 module.exports = mi;
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,162 +2,179 @@ let app = getApp();
 let wxCharts = require('../../common/js/wxcharts.js');
 let mi = require('../../common/js/mi.js');
 let lineChart = null;
-const api={
-
+const api = {
+  bindThirdAccount: mi.ip + 'user/bindThirdAccount',//绑定第三方账号
 };
 Page({
-    data: {},
-    onLoad() {
-        this.lineInit();
-        this.getUserInfo();
+  data: {
+    isAuthorize: false,//是否授权
+  },
+  onLoad() {
+    this.lineInit();
+    this.getUserInfo();
 
-    },
-    onShow() {
+  },
+  onShow() {
 
-    },
-    getUserInfo(){
-        //获取用户信息
-        wx.getSetting({
-            success: function(res){
-                if (res.authSetting['scope.userInfo']) {
-                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-                    wx.getUserInfo({
-                        success: function(res) {
-                            console.log(res.userInfo);
-                            console.log(res);
-                        }
-                    })
-                }
-            }
-        })
-    },
-    bluetoothInit: function () {
-        //检测蓝牙是否打开
-        wx.openBluetoothAdapter({
+  },
+  getUserInfo() {
+    let that = this;
+    //获取用户信息
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          that.setData({
+            isAuthorize: true
+          });
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
             success: function (res) {
-                console.log(res);
-                //检测蓝牙此时的状态
-                wx.getBluetoothAdapterState({
-                    success: function (res) {
-                        console.log(res);
-                        //监听蓝牙适配器状态
-                        wx.onBluetoothAdapterStateChange(function (res) {
-                            console.log(`adapterState changed, now is`, res);
-                        })
-                        //如果蓝牙此时处于空闲，则可以
-                        if (res.available && !res.discovering) {
-                            //开启蓝牙搜索模式
-                            wx.startBluetoothDevicesDiscovery({
-                                services: [],
-                                success: function (res) {
-                                    console.log(res)
+              console.log(res.userInfo);
+              console.log(res);
+              let openId ='oXGyD1VK6GnPVbUrcul8Wtp0FuWE';//定义openId
+              // mi.ajax({
+              //   url: api.bindThirdAccount,
+              //   method:'post',
+              //   data:{
 
-                                    // ArrayBuffer转16进度字符串示例
-                                    function ab2hex(buffer) {
-                                        let hexArr = Array.prototype.map.call(
-                                            new Uint8Array(buffer),
-                                            function (bit) {
-                                                return ('00' + bit.toString(16)).slice(-2)
-                                            }
-                                        )
-                                        return hexArr.join('');
-                                    }
+              //   },
+              //   success:function(){
 
-                                    wx.onBluetoothDeviceFound(function (devices) {
-                                        console.log('new device list has founded')
-                                        console.dir(devices)
-                                        console.log(ab2hex(devices[0].advertisData))
-                                    })
-                                },
-                                fail: function (res) {
-                                    console.log(res);
-                                }
-                            })
-
-                        }
-
-
-                    },
-                    fail: function (res) {
-                        console.log(res);
-                    }
-                });
-
-
-            },
-            fail: function (res) {
-                console.log('蓝牙未打开');
+              //   }
+              // });
             }
-        });
-    },
-    lineInit: function () {
-        let windowWidth = 320;
-        try {
-            let res = wx.getSystemInfoSync();
-            windowWidth = res.windowWidth - 20;
-        } catch (e) {
-            console.error('getSystemInfoSync failed!');
+          })
         }
+      }
+    })
+  },
+  bluetoothInit: function () {
+    //检测蓝牙是否打开
+    wx.openBluetoothAdapter({
+      success: function (res) {
+        console.log(res);
+        //检测蓝牙此时的状态
+        wx.getBluetoothAdapterState({
+          success: function (res) {
+            console.log(res);
+            //监听蓝牙适配器状态
+            wx.onBluetoothAdapterStateChange(function (res) {
+              console.log(`adapterState changed, now is`, res);
+            })
+            //如果蓝牙此时处于空闲，则可以
+            if (res.available && !res.discovering) {
+              //开启蓝牙搜索模式
+              wx.startBluetoothDevicesDiscovery({
+                services: [],
+                success: function (res) {
+                  console.log(res)
 
-        let simulationData = this.createSimulationData();
-        lineChart = new wxCharts({
-            canvasId: 'lineCanvas',
-            type: 'line',
-            categories: simulationData.categories,
-            animation: true,
-            // background: '#f5f5f5',
-            series: [{
-                name: '成交量1',
-                data: simulationData.data,
-                format: function (val, name) {
-                    return val.toFixed(2) + '万';
-                }
-            }, {
-                name: '成交量2',
-                data: [2, 0, 0, 3, null, 4, 0, 0, 2, 0],
-                format: function (val, name) {
-                    return val.toFixed(2) + '万';
-                }
-            }],
-            xAxis: {
-                disableGrid: true
-            },
-            yAxis: {
-                title: '成交金额 (万元)',
-                format: function (val) {
-                    return val.toFixed(2);
+                  // ArrayBuffer转16进度字符串示例
+                  function ab2hex(buffer) {
+                    let hexArr = Array.prototype.map.call(
+                      new Uint8Array(buffer),
+                      function (bit) {
+                        return ('00' + bit.toString(16)).slice(-2)
+                      }
+                    )
+                    return hexArr.join('');
+                  }
+
+                  wx.onBluetoothDeviceFound(function (devices) {
+                    console.log('new device list has founded')
+                    console.dir(devices)
+                    console.log(ab2hex(devices[0].advertisData))
+                  })
                 },
-                min: 0
-            },
-            width: windowWidth,
-            height: 200,
-            dataLabel: false,
-            dataPointShape: true,
-            extra: {
-                lineStyle: 'curve'
+                fail: function (res) {
+                  console.log(res);
+                }
+              })
+
             }
+
+
+          },
+          fail: function (res) {
+            console.log(res);
+          }
         });
-    },
-    touchHandler: function (e) {
-        console.log(lineChart.getCurrentDataIndex(e));
-        lineChart.showToolTip(e, {
-            // background: '#7cb5ec',
-            format: function (item, category) {
-                return category + ' ' + item.name + ':' + item.data
-            }
-        });
-    },
-    createSimulationData: function () {
-        let categories = [];
-        let data = [];
-        for (let i = 0; i < 10; i++) {
-            categories.push('2016-' + (i + 1));
-            data.push(Math.random() * (20 - 10) + 10);
+
+
+      },
+      fail: function (res) {
+        console.log('蓝牙未打开');
+      }
+    });
+  },
+  lineInit: function () {
+    let windowWidth = 320;
+    try {
+      let res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth - 20;
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
+
+    let simulationData = this.createSimulationData();
+    lineChart = new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: simulationData.categories,
+      animation: true,
+      // background: '#f5f5f5',
+      series: [{
+        name: '成交量1',
+        data: simulationData.data,
+        format: function (val, name) {
+          return val.toFixed(2) + '万';
         }
-        // data[4] = null;
-        return {
-            categories: categories,
-            data: data
+      }, {
+        name: '成交量2',
+        data: [2, 0, 0, 3, null, 4, 0, 0, 2, 0],
+        format: function (val, name) {
+          return val.toFixed(2) + '万';
         }
-    },
+      }],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        title: '成交金额 (万元)',
+        format: function (val) {
+          return val.toFixed(2);
+        },
+        min: 0
+      },
+      width: windowWidth,
+      height: 200,
+      dataLabel: false,
+      dataPointShape: true,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
+  },
+  touchHandler: function (e) {
+    console.log(lineChart.getCurrentDataIndex(e));
+    lineChart.showToolTip(e, {
+      // background: '#7cb5ec',
+      format: function (item, category) {
+        return category + ' ' + item.name + ':' + item.data
+      }
+    });
+  },
+  createSimulationData: function () {
+    let categories = [];
+    let data = [];
+    for (let i = 0; i < 10; i++) {
+      categories.push('2016-' + (i + 1));
+      data.push(Math.random() * (20 - 10) + 10);
+    }
+    // data[4] = null;
+    return {
+      categories: categories,
+      data: data
+    }
+  },
 });
