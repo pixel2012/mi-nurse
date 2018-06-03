645699a00aab99aeca6e0efe2498f5558685b791
李虎 <lihu20120810@163.com>
2018-06-03 20:36:49 +0800

    解决图标的显示和值的获取

--- common/js/mi.js
+++ common/js/mi.js
@@ -212,6 +212,23 @@ const mi = {
       }
     }
     return max;
+  },
+  toast(data) {
+    if (typeof data == 'string') {
+      wx.showToast({
+        title: data,
+        icon: 'none'
+      });
+    }
+  },
+  deepMerge(obj1, obj2) {
+    var key;
+    for (key in obj2) {
+      // 如果target(也就是obj1[key])存在，且是对象的话再去调用deepMerge，否则就是obj1[key]里面没这个对象，需要与obj2[key]合并
+      obj1[key] = obj1[key] && obj1[key].toString() === "[object Object]" ?
+        deepMerge(obj1[key], obj2[key]) : obj1[key] = obj2[key];
+    }
+    return obj1;
   }
 };
 module.exports = mi;
